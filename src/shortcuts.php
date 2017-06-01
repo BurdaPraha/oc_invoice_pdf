18a193cad763879d66476bf446053e3f7e16df6e
Michal Landsman <landsman@insuit.cz>
2017-06-01 10:34:11 +0200

    working order printing, todo: utf8

--- src/shortcuts.php
+++ src/shortcuts.php
@@ -3,24 +3,36 @@
 use Dompdf\Dompdf;
 use Dompdf\Options;
 
-function invoice_pdf($data, $name) {
+function invoice_pdf($view, $data) {
+
+    /*
+    echo "<pre>";
+    var_dump($data);
+    echo "</pre>";
+    */
+
+    $shop_name  = "{$data['name']}";
+    $orders     = $data['orders'];
 
     $options = new Options();
     $options->set('defaultFont', 'Courier');
 
-    if (count($name) > 1) {
-        $name = "Orders";
-    }else{
-        $name = 'Order_'.$name[0]['order_id'];
+    if (count($orders) > 1)
+    {
+        $name = "Orders - {$shop_name}";
+    }
+    else
+    {
+        $invoice    = $orders[0]['invoice_no']; // todo make isset to contrib
+        $order_id   = $orders[0]['order_id'];
+        $name       = "{$invoice}_{$shop_name}";
     }
 
     $pdf = new Dompdf(); // $options
-    $pdf->loadHtml($data);
+    $pdf->loadHtml($view);
 
     //$pdf->setPaper('A4', 'landscape');
 
-
-
     $pdf->render();
-    $pdf->stream($name.".pdf");
+    $pdf->stream("{$name}.pdf");
 }
